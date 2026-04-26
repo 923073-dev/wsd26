cf44343dc18d657119983ca6ea000a4be10065b1
923073 <e1923073@oit.ac.jp>
2026-04-26 16:25:59 +0900

    最終保存

--- wsd26/flutter_application_1/lib/main.dart
+++ wsd26/flutter_application_1/lib/main.dart
@@ -10,39 +10,91 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      title: 'Hisakata App',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.lightGreen)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Column(
         children: [
           Expanded(
-            flex: 1,
+            flex: 2,
             child: Row(
               children: [
+                //Expanded(flex: 2, child: Text("count: $_counter")),
                 Expanded(
-                  child: Center(
-                    child: Placeholder(),
+                  flex: 2,
+                  child: Image.network(
+                    "https://picsum.photos/seed/$_counter/400/300",
                   ),
                 ),
-
                 Expanded(
                   child: Column(
                     children: [
-                      Expanded(child: Center(child: Placeholder())),
-                      Expanded(child: Center(child: Placeholder())),
-                      Expanded(child: Center(child: Placeholder())),
-                      Expanded(child: Center(child: Placeholder())),
+                      Expanded(
+                        child: FittedBox(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _counter++;
+                              });
+                            },
+                            child: Icon(Icons.plus_one),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: FittedBox(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _counter--;
+                              });
+                            },
+                            child: Icon(Icons.exposure_minus_1_outlined),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: FittedBox(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _counter = 0;
+                              });
+                            },
+                            child: Icon(Icons.lock_reset_sharp),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ],
             ),
           ),
-
-          Expanded(
-            flex: 1,
-            child: Center(
-              child: Placeholder(),
-            ),
-          ),
+          Expanded(child: Placeholder()),
         ],
       ),
     );
